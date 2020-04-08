3888896fb7725315e68bb141a30a4cfea568f324
Brian Danowski <briandanowski@gmail.com>
2020-04-08 18:42:58 -0400

    init

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mailtruck World' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login - Mailtruck World' });
+});
+
 
 module.exports = router;

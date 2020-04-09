926b421aa6257e0ea1f2f2b8341f3421ecf4fef5
Brian Danowski <briandanowski@gmail.com>
2020-04-08 20:28:53 -0400

    login page

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login - Mailtruck World' });
+  res.render('login', { title: 'Login' });
 });
 
 
